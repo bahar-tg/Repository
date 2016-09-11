762380be01cf430fae290e460481ec31863e553b
bahar-tg <bhr.taghavi@gmail.com>
2016-09-11 15:41:00 +0430

    -

--- TradeshiftAssignment/Domain/TriangleType.cs
+++ TradeshiftAssignment/Domain/TriangleType.cs
@@ -5,7 +5,7 @@
     /// </summary>
     public enum TriangleType
     {
-        NoTriangle = 0,//Given lenghts cannot creat a triangle
+        NoTriangle = 0,//lenghts which cannot creat a triangle
         Scalene = 1,// none of the sides are the same length
         Isosceles = 2,// two sides are the same length and one differs
         Equilateral = 3 // all sides are the same length
